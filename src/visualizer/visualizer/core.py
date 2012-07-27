a6d8c55d8a3d9e852d3a2a115b7642df41667561
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2012-07-26 17:58:42 -0700

    visualizer+mobility+bindings: A specialized version of GetObject (GetMobilityModel) in MobilityModel
    This method is now used by the visualizer to obtain mobility interface.
    
    This trick is necessary on some platforms in order to use mobility models that
    are not explicitly exported (or is impossible to export) to python.

--- src/visualizer/visualizer/core.py
+++ src/visualizer/visualizer/core.py
@@ -236,7 +236,7 @@ class Node(PyVizObject):
 
             self.emit("query-extra-tooltip-info", lines)
 
-            mob = ns3_node.GetObject(ns.mobility.MobilityModel.GetTypeId())
+            mob = ns.mobility.MobilityModel.GetMobilityModel (ns3_node)
             if mob is not None:
                 lines.append('  <b>Mobility Model</b>: %s' % mob.GetInstanceTypeId().GetName())
 
@@ -476,7 +476,7 @@ class Node(PyVizObject):
         """
         if self._has_mobility is None:
             node = ns.network.NodeList.GetNode(self.node_index)
-            mobility = node.GetObject(ns.mobility.MobilityModel.GetTypeId())
+            mobility = ns.mobility.MobilityModel.GetMobilityModel (node)
             self._has_mobility = (mobility is not None)
         return self._has_mobility
 
@@ -1069,7 +1069,9 @@ class Visualizer(gobject.GObject):
             node_name = "Node %i" % nodeI
             node_view = self.get_node(nodeI)
 
-            mobility = node.GetObject(ns.mobility.MobilityModel.GetTypeId())
+            mobility = ns.mobility.MobilityModel.GetMobilityModel (node)
+
+            # print "Mobility type: " + mobility.GetInstanceTypeId().GetName()
             if mobility is not None:
                 node_view.set_color("red")
                 pos = mobility.GetPosition()
@@ -1130,7 +1132,7 @@ class Visualizer(gobject.GObject):
 
         print "scanning topology: all done."
         self.emit("topology-scanned")
-
+        
     def get_node(self, index):
         try:
             return self.nodes[index]
@@ -1176,7 +1178,7 @@ class Visualizer(gobject.GObject):
         for node in self.nodes.itervalues():
             if node.has_mobility:
                 ns3_node = ns.network.NodeList.GetNode(node.node_index)
-                mobility = ns3_node.GetObject(ns.mobility.MobilityModel.GetTypeId())
+                mobility = ns.mobility.MobilityModel.GetMobilityModel (ns3_node)
                 if mobility is not None:
                     pos = mobility.GetPosition()
                     x, y = transform_point_simulation_to_canvas(pos.x, pos.y)
@@ -1529,7 +1531,7 @@ class Visualizer(gobject.GObject):
         self.simulation.lock.acquire()
         try:
             ns3_node = ns.network.NodeList.GetNode(node.node_index)
-            mob = ns3_node.GetObject(ns.mobility.MobilityModel.GetTypeId())
+            mob = ns.mobility.MobilityModel.GetMobilityModel (ns3_node)
             if mob is None:
                 return
             if self.node_drag_state is not None:
@@ -1546,7 +1548,7 @@ class Visualizer(gobject.GObject):
         self.simulation.lock.acquire()
         try:
             ns3_node = ns.network.NodeList.GetNode(node.node_index)
-            mob = ns3_node.GetObject(ns.mobility.MobilityModel.GetTypeId())
+            mob = ns.mobility.MobilityModel.GetMobilityModel (ns3_node)
             if mob is None:
                 return False
             if self.node_drag_state is None:
